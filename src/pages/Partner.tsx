5681f40c4de1fb088a5e0aa142a0fd7d01f8150d
Louran Costa <louran.costa@yahoo.com.br>
2024-08-02 20:35:47 -0300

    feat: update partner by form

--- src/pages/Partner.tsx
+++ src/pages/Partner.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { FormEvent, useEffect, useState } from "react";
 import "../styles/pages/partner.css";
 import Sidebar from "../components/Sidebar";
 import api from "../services/api";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface Partner {
   name: string;
@@ -18,16 +19,48 @@ interface PartnerParams {
 
 export default function GetPartner() {
   const params = useParams<PartnerParams | any>();
+
   const [partner, setPartner] = useState<Partner>();
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [phone, setPhone] = useState("");
+  const [domain, setDomain] = useState("");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     api.get(`/partners/${params.id}`).then((response) => {
       setPartner(response.data);
+      setName(response.data.name);
+      setCity(response.data.city);
+      setCountry(response.data.country);
+      setPhone(response.data.phone);
+      setDomain(response.data.domain);
     });
   }, [params.id]);
 
   if (!partner) {
     return <p>Carregando...</p>;
+  }
+
+  async function handleUpdate(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      name: name,
+      domain: domain,
+      phone: phone,
+      city: city,
+      country: country,
+    };
+
+    await api.patch(`/partners/${params.id}`, JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+    });
+
+    alert("Cadastro atualizado com sucesso!");
+    navigate("/partners");
   }
 
   return (
@@ -40,26 +73,55 @@ export default function GetPartner() {
             <legend>Informações do Parceiro</legend>
             <div className="input-block">
               <label htmlFor="name">Nome</label>
-              <input id="name" defaultValue={partner.name} />
+              <input
+                id="name"
+                defaultValue={partner.name}
+                onChange={(event) => setName(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="city">Cidade</label>
-              <input id="city" defaultValue={partner.city} />
+              <input
+                id="city"
+                defaultValue={partner.city}
+                onChange={(event) => setCity(event.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="country">País</label>
+              <input
+                id="country"
+                defaultValue={partner.country}
+                onChange={(event) => setCountry(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="phone">Telefone</label>
-              <input id="phone" defaultValue={partner.phone} />
+              <input
+                id="phone"
+                defaultValue={partner.phone}
+                onChange={(event) => setPhone(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
-              <label htmlFor="website">WebSite</label>
-              <input id="website" defaultValue={partner.domain} />
+              <label htmlFor="domain">WebSite</label>
+              <input
+                id="domain"
+                defaultValue={partner.domain}
+                onChange={(event) => setDomain(event.target.value)}
+              />
             </div>
           </fieldset>
 
-          <button className="update-button" type="submit">
+          <button
+            className="update-button"
+            type="submit"
+            onClick={handleUpdate}
+          >
             Atualizar
           </button>
 
